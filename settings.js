f00fcccd4230105cb1d0f5a9867e7c284f0a0a38
Netherzin <randomnether13@gmail.com>
2025-03-24 17:23:54 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Nether Bot",
+  botOwner: 'Nether Player13', // Your name
+  ownerNumber: '+258877791178', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
